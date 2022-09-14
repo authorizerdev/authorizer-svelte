fd87ec16998d8830b091901854c28281c18d517f
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-09-14 16:52:04 +0530

    fix(example): update login style

--- example/src/routes/Login.svelte
+++ example/src/routes/Login.svelte
@@ -2,10 +2,17 @@
   import { Authorizer } from '../../../src'
 </script>
 
-<div
-  style="display: flex; flex-direction: column; justify-content: center; align-items: center;"
->
+<div class="login-container">
   <h1>Welcome to Authorizer</h1>
   <br />
   <Authorizer />
 </div>
+
+<style>
+  .login-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
