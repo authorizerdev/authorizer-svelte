ec71722c74cf378f50c41c60a06a225e61bab444
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-09-14 17:01:44 +0530

    fix: add twitter login

--- src/components/AuthorizerSocialLogin.svelte
+++ src/components/AuthorizerSocialLogin.svelte
@@ -1,6 +1,6 @@
 <script>
   import { StyledButton, StyledSeparator } from '../styledComponents'
-  import { Google, Github, Facebook, Linkedin, Apple } from '../icons'
+  import { Google, Github, Facebook, Linkedin, Apple, Twitter } from '../icons'
   import { store } from '../store'
   import { createQueryParams } from '../utils/common'
 
@@ -19,7 +19,8 @@
     state.config.is_github_login_enabled ||
     state.config.is_facebook_login_enabled ||
     state.config.is_linkedin_login_enabled ||
-    state.config.is_apple_login_enabled
+    state.config.is_apple_login_enabled ||
+    state.config.is_twitter_login_enabled
   $: queryParams = createQueryParams({
     ...urlProps,
     scope: urlProps.scope.join(' ')
@@ -86,6 +87,18 @@
     >
       <Linkedin />
       Sign in with Linkedin
+    </StyledButton>
+    <br />
+  {/if}
+  {#if state.config.is_twitter_login_enabled}
+    <StyledButton
+      on:click={() => {
+        if (window?.location)
+          window.location.href = `${state.config.authorizerURL}/oauth_login/twitter?${queryParams}`
+      }}
+    >
+      <Twitter />
+      Sign in with Twitter
     </StyledButton>
     <br />
   {/if}
